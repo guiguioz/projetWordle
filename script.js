51d7fa114246251b1fbd1ce1733f123721b32fa6
emualliug <giyom69@hotmail.fr>
2023-01-05 16:29:42 +0100

    js3

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 console.log("hello")
 
-const dico = [
-    "pomme",
-    "homme",
-    "femme",
-    "prout",
-    "sapin",
-    "jouet",
-    "chien",
-    "sexes",
-    "route",
-    "neige"
-]
-console.log(dico)
+let instruction = document.querySelector("#instruction")
+let bouton = document.querySelector("#bouton")
+let motSaisie = document.querySelector("#mot")
+let lettreDiv = document.querySelectorAll("#lettreDiv")
+let lettre = document.querySelectorAll("#lettre")
+
+const bdd = 
+    "pomme,homme,femme,prout,sapin,jouet,chien,sexes,route,neige"
+
+console.log(bdd)
+
+let dico = bdd.split(',')
+
+console.log(dico[3])
+
+
+for (let i = 0; i < dico.length; i++) {
+    Math.floor(Math.random()*dico[i])
+    }
+
+
+
+
