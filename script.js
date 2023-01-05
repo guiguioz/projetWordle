8505a15d211bad40d7e02f2d9bd3dee01367aaec
emualliug <giyom69@hotmail.fr>
2023-01-05 16:59:23 +0100

    js5

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 console.log("hello")
 
-let instruction = document.querySelector("#instruction")
 let bouton = document.querySelector("#bouton")
-let motSaisie = document.querySelector("#mot")
+let motSaisie = document.querySelector("#texte")
 let lettreDiv = document.querySelectorAll("#lettreDiv")
 let lettre = document.querySelectorAll("#lettre")
 
 const bdd = 
-    "pomme,homme,femme,prout,sapin,jouet,chien,sexes,route,neige"
+    ["pomme","homme","femme","prout","sapin","jouet","chien","sexes","route","neige"]
 
 console.log(bdd)
 
-let dico = bdd.split(',')
+function motAleatoire(){
+    const rdm = Math.floor(Math.random()*bdd.length)
+    console.log(rdm)
+    return bdd[rdm]
+}
+const motGenere = motAleatoire()
+console.log(motGenere)
 
-console.log(dico[3])
+bouton.addEventListener("click",valideSaisie)
+
+function valideSaisie(){
+    motSaisie.value = lettre[0].textContent
+}
 
 
-for (let i = 0; i < dico.length; i++) {
-    Math.floor(Math.random()*dico[i])
-    }
-
-
+const newd = bdd.map(x => x.split(''));
+console.log(newd)
 
 
