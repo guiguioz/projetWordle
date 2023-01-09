66e37232a11efc75409ca07c627b19aa0e0bbbf5
Quentin <118888713+QuentinB0@users.noreply.github.com>
2023-01-09 16:04:57 +0100

    remise en arrière modal

--- script.js
+++ script.js
@@ -4,39 +4,33 @@ let bouton = document.querySelector("#bouton")
 let motSaisie = document.querySelector("#texte")
 let lettreDiv = document.querySelectorAll("#lettreDiv")
 let lettre = document.querySelectorAll("#lettre")
+let valider = document.querySelector("#boutonValider")
 
-
-const bdd = 
-    ["pomme","homme","femme","prout","sapin","jouet","chien","sexes","route","neige"]
+const bdd =
+    ["pomme", "homme", "femme", "prout", "sapin", "jouet", "chien", "sexes", "route", "neige"]
 
 console.log(bdd)
 
-function motAleatoire(){
-    const rdm = Math.floor(Math.random()*bdd.length)
+function motAleatoire() {
+    const rdm = Math.floor(Math.random() * bdd.length)
     console.log(rdm)
     return bdd[rdm]
 }
 const motGenere = motAleatoire()
 console.log(motGenere)
 
-bouton.addEventListener("click",valideSaisie)
-
-document.addEventListener("keyup", function(event) {
-    if (event.key === 'Enter') {
-        valideSaisie("#boutonValider").click()
-        console.log('Enter is pressed!')
-    }
-});
- 
-document.getElementById("submit").onclick = function() {
-    alert('Clicked!');
-}
-
-function valideSaisie(){
-    motSaisie.value = lettre[0].textContent
-}
-
+// bouton.addEventListener("click", valideSaisie)
 
 const newd = bdd.map(x => x.split(''));
 console.log(newd)
 
+
+let input = document.querySelector("#boutonValider");
+motSaisie.addEventListener("keypress", function (event) {
+    if (event.key === 13) {
+        document.getElementById("boutonValider").click()
+        console.log("enter",event.key)
+        // event.preventDefault();
+        // document.querySelector("#boutonValider").click();
+    }
+})
